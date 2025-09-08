02c06dae17114a3d0bda7b5815e92b5ba4a7d998
Trxif <rainertahker888@gmail.com>
2025-09-09 02:21:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ const quotes = [
     "When you hear a random noise, then it's a plane",
     "Sigma Sigma boy Sigma boy",
 ]
+
+const usedIndexes = new Set()
+const quoteElement = document.getElementById("quote")
+
+function generateQuote() {
+
+    if (usedIndexes.size >= quotes.length) {
+        usedIndexes.clear()
+    }
+
+    while (true) {
+        const randomIdx = Math.floor(Math.random() * quotes.length)
+    if (usedIndexes.has(randomIdx)) continue
+    const quote = quotes[randomIdx]
+    quoteElement.innerHTML = quote;
+    usedIndexes.add(randomIdx)
+    break
+    }
+}
